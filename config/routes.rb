3aa6f11b3f16c35de6f396577681d775200a77b5
Gustavo Seganfredo <gustavosf@gmail.com>
2014-12-25 19:37:50 -0200

    Ajustes nos menus

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,15 @@ Rails.application.routes.draw do
 
   # Gallery
   get 'gallery', to: 'gallery#index'
-  get 'gallery/:id', to: 'gallery#show'
-  get 'gallery/:id/page/:offset', to: 'gallery#page'
+  get 'gallery/:slug', to: 'gallery#show'
+  get 'gallery/:slug/page/:offset', to: 'gallery#page'
 
-  get 'home/index'
+  root 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
